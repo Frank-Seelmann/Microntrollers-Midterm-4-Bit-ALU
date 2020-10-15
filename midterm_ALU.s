e2262e936705b2e09a1a7183bb08d64d1768d77d
Frank-Seelmann <72896772+Frank-Seelmann@users.noreply.github.com>
2020-10-14 23:26:31 -0400

    Update midterm_ALU.s

--- midterm_ALU.s
+++ midterm_ALU.s
@@ -1,5 +1,17 @@
 ; midterm_ALU.s 
-
+;
+; Frank Seelmann		EGC331-01 - Microcontrollers System Design
+; Prof Michael Otis		10/14/20
+;
+; The ALU takes input in three stages, the first being the first four-bit number, 
+; the second being the selector for the arithmetic/logic operation being performed, 
+; and the final being the second four-bit number. All of these inputs are taken 
+; sequentially using the same 4 switch dip-switch, where a button on the NUCLEO 
+; board is used to confirm the input and move on to the next stage. Since internal 
+; pull-up resistors are used with the switches, the inputs are then inverted to make 
+; them easier to work with, then have everything except the least significant nibble 
+; masked out for all three. At this stage a conditional ladder is used to determine
+; which operation to perform based on the operation input. 
 
             EXPORT  __Vectors
             EXPORT  Reset_Handler
